49b9e6787a9730ffeaa261a6e1bc0cd4f40565d1
Juan Carlos Chavez <lolzthisismyemail@gmail.com>
2015-05-01 08:43:37 -0700

    Lab2 Size Change
    Changed the size of the result because 4 bits longer

--- lab2/lab2_alu/my_alu.vhd
+++ lab2/lab2_alu/my_alu.vhd
@@ -25,62 +25,85 @@ use ieee.std_logic_unsigned.all;
 
 entity my_alu is
 
-	generic(NUMBITS: natural:=4); --Specifies the operation's width
+	generic(NUMBITS: natural:=32); --Specifies the operation's width
 	
     Port ( 
 				--Size is NUMBITS-1, the last bit found in NUMBITS is reserved for the carryout
 				
 				A 			: in   STD_LOGIC_VECTOR(NUMBITS-1 downto 0);
 				B 			: in   STD_LOGIC_VECTOR(NUMBITS-1 downto 0);
-				opcode   : in   STD_LOGIC_VECTOR(3 downto 0); --Lab2: opcode now 4 bits
-				result   : out  STD_LOGIC_VECTOR(NUMBITS-1 downto 0);
+				opcode   : in   STD_LOGIC_VECTOR(3 downto 0); --<Lab2>: opcode now 4 bits
+				result   : out  STD_LOGIC_VECTOR(NUMBITS+3 downto 0); --<Lab2>: result 4 bits longer than inputs A&B
 				carryout : out  STD_LOGIC; --Only matters with unsigned
 				overflow : out  STD_LOGIC; --Only matters with singed
 				zero 		: out  STD_LOGIC	
 				
 		    );
 			  		  
+					  
+	--<Lab2>: Function decoder for binary decimal 
+	
+	--function bcd_decode (bcd_input : std_logic_vector(NUMBITS-1 downto 0);
+								--decode_opcode : std_logic_vector)
+	--return integer is
+	
+	--variable current_place : integer := 0; --<Lab2>: goes by fours (for every nibble)
+	--variable exponent_dec : integer := 0; --<Lab2>: increases by one each time
+	--variable nibble : integer := std_logic_vector(3 downto 0); --<Lab2>: stores nibble
+	--variable decimal_form : integer := 0; --<Lab2>: converted decimal form
+
+
+	--end bcd_decode
+	
 end my_alu;
 
 architecture behavioral of my_alu is
 	-- Store intermediate value
-	signal tmp: std_logic_vector(NUMBITS downto 0);
+	signal tmp: std_logic_vector(NUMBITS+3 downto 0); --<Lab2>: because result is 4 bits longer (?)
 	
 begin
 
 	process(opcode, tmp, A, B)
-
+				-- Variable declaration
+				variable bcd : integer := 0;
+				
 		begin
+				
+			--Convert A and B from BCD to Decimal			
 			
 			case opcode is
-			
+				-- <Lab2>:
+				-- Convert A and B from BCD inputs to decimal (?)
+				
 				-- Unsigned add_____________________________________________________________________________________________________
 				
 				when "1000" =>
-				
-					tmp <= std_logic_vector(('0' & A) + ('0' & B));
 					
-					result <= tmp(NUMBITS-1 downto 0);
+					
+					-- tmp <= std_logic_vector(('0' & A) + ('0' & B));
+					
+					
+					-- reslt <= tmp(NUMBITS-1 downto 0);
 					
 					-- Only carryout matters for unsigned, overflow should be turned off
-					carryout <= tmp(NUMBITS);
-					overflow <= tmp(NUMBITS);
+					-- carryout <= tmp(NUMBITS);
+					-- overflow <= tmp(NUMBITS);
 					
 					-- Set the zero flag accordingly
-					if(tmp(NUMBITS-1 downto 0)(NUMBITS-1 downto 0) = 0) then
-						zero <= '1';
-					else
-						zero <= '0';
-					end if;	
+					-- if(tmp(NUMBITS-1 downto 0)(NUMBITS-1 downto 0) = 0) then
+					--	zero <= '1';
+					-- else
+					--	zero <= '0';
+					-- end if;	
 					
 				-- Signed add_______________________________________________________________________________________________________
 				
 				when "1100" =>
 				
-					tmp <= std_logic_vector(signed(A(NUMBITS - 1)&A) 
-					+ signed(B(NUMBITS - 1)&B));
+					-- tmp <= std_logic_vector(signed(A(NUMBITS - 1)&A) 
+					-- + signed(B(NUMBITS - 1)&B));
 					
-					result <= tmp(NUMBITS-1 downto 0);
+					result <= tmp(NUMBITS+3 downto 0); --<Lab2>: because result is 4 bits longer (?)
 					
 					-- The cases in which the overflow flag will be set for signed add
                if((signed(A) >= 0 and signed(B) >= 0) and (signed(tmp(NUMBITS-1 downto 0)) < 0)) or
@@ -102,9 +125,9 @@ begin
 				
 				when "1001" =>
 				
-					tmp <= std_logic_vector(('0' & A) - ('0' & B));
+					-- tmp <= std_logic_vector(('0' & A) - ('0' & B));
 					
-					result <= tmp(NUMBITS-1 downto 0);
+					result <= tmp(NUMBITS+3 downto 0); --<Lab2>: because result is 4 bits longer (?)
 					
 					carryout <= not tmp(NUMBITS);
 					overflow <= tmp(NUMBITS);
@@ -119,10 +142,10 @@ begin
 				
 				when "1101" =>
 				
-					tmp <= std_logic_vector(signed(A(NUMBITS - 1)&A) 
-					- signed(B(NUMBITS - 1)&B));
+					-- tmp <= std_logic_vector(signed(A(NUMBITS - 1)&A) 
+					-- - signed(B(NUMBITS - 1)&B));
 					
-					result <= tmp(NUMBITS-1 downto 0);
+					result <= tmp(NUMBITS+3 downto 0); --<Lab2>: because result is 4 bits longer (?)
 					
 					if ((signed(A) >= 0 and signed(B) < 0) and (signed(tmp(NUMBITS-1 downto 0)) < 0)) or
                ((signed(A) < 0 and signed(B) >= 0) and (signed(tmp(NUMBITS-1 downto 0)) >= 0))
@@ -142,7 +165,7 @@ begin
 				
 				when others =>
 				
-					result <= tmp(NUMBITS-1 downto 0);
+					result <= tmp(NUMBITS+3 downto 0); --<Lab2>: because result is 4 bits longer (?)
 				
 			end case;
 			
--- lab2/lab2_alu/my_alu_tb.vhd
+++ lab2/lab2_alu/my_alu_tb.vhd
@@ -50,7 +50,7 @@ ARCHITECTURE behavior OF my_alu_tb IS
     PORT(
 				A 			: IN  std_logic_vector(NUMBITS-1 downto 0);
 				B 			: IN  std_logic_vector(NUMBITS-1 downto 0);
-				opcode 	: IN  std_logic_vector(3 downto 0); --Lab2: opcode now 4 bits
+				opcode 	: IN  std_logic_vector(3 downto 0); --<Lab2>: opcode now 4 bits
 				result 	: OUT  std_logic_vector(NUMBITS-1 downto 0);
 				carryout : OUT  std_logic;
 				overflow : OUT  std_logic;
