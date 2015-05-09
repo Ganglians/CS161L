55a75a6f34aea92613e9306f8b3babae4be50d27
shilpa <shilparoys@yahoo.com>
2015-05-09 16:44:03 -0700

    finished (1) control_unit

--- lab4/inc/control_unit.vhd
+++ lab4/inc/control_unit.vhd
@@ -19,5 +19,59 @@ end control_unit;
 architecture Behavioral of control_unit is
 
 begin
-  
+    process(instr_op)
+    begin
+        case instr_op is
+            --R-format
+            when "000000" => 
+                reg_dest   <= '1';
+                alu_src    <= '0';
+                mem_to_reg <= '0';
+                reg_write  <= '1';
+                mem_read   <= '0';
+                mem_write  <= '0';
+                branch     <= '0';
+                alu_op     <= "10';
+            --lw-format
+            when "100011" => 
+                reg_dest   <= '0';
+                alu_src    <= '1';
+                mem_to_reg <= '1';
+                reg_write  <= '1';
+                mem_read   <= '1';
+                mem_write  <= '0';
+                branch     <= '0';
+                alu_op     <= "00';
+             --sw-format
+            when "101011" => 
+                reg_dest   <= '0';
+                alu_src    <= '1';
+                mem_to_reg <= '0';
+                reg_write  <= '0';
+                mem_read   <= '0';
+                mem_write  <= '1';
+                branch     <= '0';
+                alu_op     <= "00';
+            --beq-format
+            when "000100" => 
+                reg_dest   <= '0';
+                alu_src    <= '0';
+                mem_to_reg <= '0';
+                reg_write  <= '0';
+                mem_read   <= '0';
+                mem_write  <= '0';
+                branch     <= '1';
+                alu_op     <= "01';
+             --default
+            when others => 
+                reg_dest   <= '0';
+                alu_src    <= '0';
+                mem_to_reg <= '0';
+                reg_write  <= '0';
+                mem_read   <= '0';
+                mem_write  <= '0';
+                branch     <= '0';
+                alu_op     <= "00';
+       end case;
+    end process;
 end Behavioral;
