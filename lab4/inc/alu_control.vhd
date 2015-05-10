0eff9cf81b09f1ac835f0419a528a6fe43597014
shilpa <shilparoys@yahoo.com>
2015-05-09 17:01:07 -0700

    finished (1) alu_unit

--- lab4/inc/alu_control.vhd
+++ lab4/inc/alu_control.vhd
@@ -13,5 +13,40 @@ end alu_control;
 architecture Behavioral of alu_control is
 
 begin
-
-end Behavioral;
+    process (alu_op, instruction_5_0)
+    begin
+        case alu_op is
+            --LW & SW--
+            when "00" =>
+                alu_out <= "0010";
+            --branch equal--
+            when "01" =>
+                alu_out <= "0010";
+            -- r-type -- 
+            when "10" =>
+                case instruction_5_0 is
+                    --add
+                    when "100000" =>
+                        alu_out <= "0010";
+                    --subtract
+                    when "100010" =>
+                        alu_out <= "0110";
+                    --and
+                    when "100100" =>
+                        alu_out <= "0000";
+                    --or
+                    when "100101" =>
+                        alu_out <= "0001";
+                    --set on less than
+                    when "101010" =>
+                        alu_out <= "0111";
+                    --others
+                    when others =>
+                        alu_out <= "0000";
+                end case;
+            --others
+            when others =>
+                alu_out <= "0000";
+        end case;
+    end process;
+end Behavioral; 
